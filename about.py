310dfa89fea9fb752425da36c4c7617d6d11e351
Shantanu Zadbuke <51382138+7shantanu7@users.noreply.github.com>
2021-05-22 00:40:48 +0530

    Update about.py

--- about.py
+++ about.py
@@ -3,12 +3,9 @@ import streamlit as st
 def write():
     st.image("stock-market.webp", width=500)
     st.title("Stock Prediction WebApp Using Machine Learning")
-    #st.title("Machine Learning")
     st.markdown("---")
     st.subheader("A simple webapp to predict next day stock price using machine learning")
     st.markdown("---")
-  
-    
     st.success("**Click the Company Info/Finance to begin**")
     st.text("")
     st.text("")
