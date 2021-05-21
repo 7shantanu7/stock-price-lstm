28925ea59924ab0d7e7687672c7b09f856bc01f5
Shantanu Zadbuke <51382138+7shantanu7@users.noreply.github.com>
2021-05-22 00:17:11 +0530

    Add files via upload

---
+++ about.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+def write():
+    st.image("stock-market.webp", width=500)
+    st.title("Stock Prediction WebApp Using Machine Learning")
+    #st.title("Machine Learning")
+    st.markdown("---")
+    st.subheader("A simple webapp to predict next day stock price using machine learning")
+    st.markdown("---")
+  
+    
+    st.success("**Click the Company Info/Finance to begin**")
+    st.text("")
+    st.text("")
+    st.text("")
+    st.text("")
+    st.text("A group project made by:")
+    st.text("Kunal Sirpor, Pranav Virmalwar & Shantanu Zadbuke")
