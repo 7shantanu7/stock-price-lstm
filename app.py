c5fa7f07ce0cf94191038c8e0e5b293a6a795c43
Shantanu Zadbuke <51382138+7shantanu7@users.noreply.github.com>
2021-05-23 21:08:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,12 +7,9 @@ import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense, LSTM
 from sklearn import metrics
-import matplotlib.pyplot as plt
 
 def write():
     #st.text("(note some companies may not available, we're working on it)")
-    st.text("Tip: Use Ctrl+F for searching")
-    st.title("Company")
     df = get_nse_equity()
 
     symbol_lst = get_symbol_lst()
@@ -22,30 +19,36 @@ def write():
     if stock:
         if st.sidebar.button("Choose another company"):
             stock=""
-            
+    
+    if stock=="":
+        st.title("Listed Companies and their Symbols")
+        st.text("Tip: Use Ctrl+F for searching")
+        
     stock_lst = df
-
+    
     # Check the symbol if it ends with .NS then it's yahoo symbol
     if stock in symbol_lst:
         stock = stock
     elif stock in new_symbol_lst:
         stock = stock + ".NS"
 
-
     if stock == "":
         st.table(stock_lst[["NSE SYMBOL","NAME OF COMPANY","YAHOO SYMBOL"]].reset_index(drop=True))
     else:
         company_info = get_company_info(stock)
-        st.write(f"Industry: {company_info['industry']}")
-        business_summary = st.checkbox("Business Summary")
-        if business_summary:
+        st.title(company_info['longName'])
+        st.subheader(f"Industry: {company_info['industry']}")
+        #Business Summary
+        business_information = st.checkbox("Business Summary")
+        if business_information:
             st.write(company_info['longBusinessSummary'])
-        contact_info = st.checkbox("Contact Info")
-        if contact_info:
+        contact_information = st.checkbox("Contact Info")
+        #Company Contact Info
+        if contact_information:
             st.write(f"City: {company_info['city']}")
             phone = company_info['phone'].replace(" ","")
             phone = "+"+phone
-            st.text(f"Phone: {phone}")
+            st.write(f"Phone: {phone}")
             st.write(f"Website: {company_info['website']}")
 
         st.sidebar.header("Finance")
@@ -53,7 +56,7 @@ def write():
 
         if market_info:
             st.subheader("Open - High - Low - Close")
-            st.text("1 Month History Data")
+            st.text("1 Month Historical Data")
             time_range = "1mo"
             st.dataframe(stock_ohlc(stock, time_range))
         stock_pred = st.sidebar.checkbox("Stock Price Prediction")
@@ -65,14 +68,14 @@ def write():
                 return option[item]
             word = st.selectbox('Select a feature you want to predict', options=list(option.keys()))
             
-            num_days= st.text_input("Enter a number of days", "1")
+            num_days= st.text_input("Enter number of days based upon which the model would train", "1")
             num_days = int(num_days)
-            st.info(f"Predict the {format_func(word)} price of the next deal day based on past {num_days} days")
+            st.info(f"Predict the {format_func(word)} Price of the next deal day based on past {num_days} days")
 
             df = get_stock_history(stock)
             df_new = df[[word]]
             dataset = df_new.values
-            #test:train = 3:7
+            #test:30% train:70%
             training_data_len = math.ceil(len(dataset) *.7)
             scaler = MinMaxScaler(feature_range=(0, 1))
             scaled_data = scaler.fit_transform(dataset)
@@ -92,10 +95,10 @@ def write():
             lstm.add(Dense(1))
             lstm.compile(loss='mean_squared_error', optimizer='adam')
 
-            if st.button('Train the model'):
+            if st.button('Train the model!'):
                 with st.spinner("Training may take time based on number of days. Please wait..."):
                     history_lstm = lstm.fit(x_train, y_train, epochs=25, batch_size=10, verbose=2)
-                    st.success("Model is ready!")
+                    st.info("Model is ready!")
                 st.button("Re-run")
 
                 test_data = scaled_data[training_data_len - num_days: , : ]
@@ -113,11 +116,11 @@ def write():
                 inputs = df_new.iloc[len(df_new)-num_days:len(df_new)].values
                 inputs = inputs.reshape(-1,1)
                 inputs  = scaler.fit_transform(inputs)
-                p = inputs.reshape(1,num_days,1)#(Samples,time_step,features)
+                p = inputs.reshape(1,num_days,1)
                 result = lstm.predict(p)
                 result = scaler.inverse_transform(result)
                 st.write("The {} price of next deal day is :".format(word))
-                st.info(round(float(result),2))
+                st.success(round(float(result),2))
 
                 st.header("Model Evaluation")
                 st.write('R Square - ',round(metrics.r2_score(y_test,pred),2))
